8c8470672b9d4ed03c5b92d3bc39e0c0b5c1cacd
Luke <lukelalalala@gmail.com>
2018-11-20 13:56:52 +1100

    script now takes full page screenshot

--- screenshotAutomation.py
+++ screenshotAutomation.py
@@ -7,15 +7,14 @@ import threading, os, time
 
 # Setup the Sheets API
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-store = file.Storage('credentials.json')
+store = file.Storage('token.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
-    creds = tools.run_flow(flow, store)
+	flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+	creds = tools.run_flow(flow, store)
 service = build('sheets', 'v4', http=creds.authorize(Http()))
-
 # spread sheet id is the value between the "/d/" and the "/edit" in the URL
-SPREADSHEET_ID = '<INSERT SHEET ID HERE>'
+SPREADSHEET_ID = '11BufuJbbUsgmauYOIsIUVECRMFz4qDine8ZYW5h1st8'
 
 # read domains
 domainsRange = 'domains'
@@ -27,29 +26,44 @@ os.makedirs('/screenshots', exist_ok=True)
 # chrome driver is needed for automated testing and it will be in headless mode
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
+options.add_argument("log-level=3")
 scrollbarWidth = 17
-targetWidth = 1920
-targetHeight = 1080
+targetWidth = None
+targetHeight = None
 
 def takeScreenshot(page):
-    browser = webdriver.Chrome(executable_path='<INSERT CHROMEDRIVER PATH HERE>',chrome_options=options)
-    browser.set_window_size(targetWidth + scrollbarWidth, targetHeight)
-    browser.get(page)
+	browser = webdriver.Chrome(executable_path='/mnt/c/Users/Luke/Desktop/chromedriver.exe',chrome_options=options)
+	original_size = browser.get_window_size()
+	filename = 'screenshots/{domain}.png'.format(domain=page[len('https://'):])
+	if targetWidth is not None and targetHeight is not None:
+		browser.set_window_size(targetWidth + scrollbarWidth, targetHeight)
 
-    # wait until page is completely loaded
-    time.sleep(3)
-    filename = 'screenshots/{domain}.png'.format(domain=page[len('https://'):])
-    browser.save_screenshot(filename)
-    Image.open(filename).crop((0, 0, targetWidth, targetHeight)).save(filename)
+	else:
+		required_width = browser.execute_script('return document.body.parentNode.scrollWidth')
+		required_height = browser.execute_script('return document.body.parentNode.scrollHeight')
+		browser.set_window_size(required_width, required_height)
+
+	browser.get(page)
+
+	# wait until page is completely loaded
+	time.sleep(3)
+
+	browser.save_screenshot(filename)
+    # avoids scrollbar
+	if targetWidth is None and targetHeight is None:
+		browser.find_element_by_tag_name('body').screenshot(filename)
+		browser.set_window_size(original_size['width'], original_size['height'])
+	else:
+		Image.open(filename).crop((0, 0, targetWidth, targetHeight)).save(filename)
 
 threadPool = []
-for page in readResult.get('values')[0]:
-    threadPool.append(threading.Thread(name=page, target=takeScreenshot, args=(page,)))
+for page in pages:
+	threadPool.append(threading.Thread(name=page, target=takeScreenshot, args=(page,)))
 
 for thread in threadPool:
-    print('Starting {name}.'.format(name=thread.name))
-    thread.start()
+	print('Starting {name}.'.format(name=thread.name))
+	thread.start()
 
 for thread in threadPool:
-    print('Joining {name}.'.format(name=thread.name))
-    thread.join()
+	print('Joining {name}.'.format(name=thread.name))
+	thread.join()
